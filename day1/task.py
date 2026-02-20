3aecc2ce51f4ad676a28b5cbe6c76c836930b82f
Nitesh Kumar <nk424353@gmail.com>
2026-02-20 08:33:11 +0530

    MAchine Learning

---
+++ day1/task.py
@@ -0,0 +1,19 @@
+# list=[]
+# for i in range(4):
+#     list.append(input("enter_num:"))
+# print(lis
+# 
+# t)
+
+import numpy as np
+# arr = np.array([10,20,30,40])
+# print(arr[1])
+# print(arr[1:4])
+
+# arr2 = np.array([[1,2],[3,4]])
+# print(arr2[0,1])
+arr = np.array([1,2,3,4])
+arr.reshape(2,2)
+print(arr)
+
+
